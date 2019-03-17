394fd18c41b43f6887050a6247a401982d9e8c5a
utish <utishrajk@gmail.com>
2019-03-17 11:14:43 -0400

    Add model classes

---
+++ src/main/java/com/example/kafkatwitterconsumer/model/Tweet.java
@@ -0,0 +1,19 @@
+package com.example.kafkatwitterconsumer.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Tweet {
+
+    private long id;
+    private String text;
+    private String lang;
+    private User user;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+}
